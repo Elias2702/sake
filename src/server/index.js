a8b37d8d909e849002a98895bd475ccdf236de92
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2019-02-20 15:42:43 +0100

    Test socket.io

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,15 @@
-/* becodeorg/bookshelf
- *
- * /src/server/index.js - Server entry point
- *
- * coded by leny@BeCode
- * started at 18/01/2019
- */
 
+import cors from "cors";
 import express from "express";
 import path from "path";
 
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 const {APP_PORT} = process.env;
 
-const app = express();
+app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
@@ -20,6 +18,13 @@ app.get("/hello", (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(APP_PORT, () =>
+server.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});

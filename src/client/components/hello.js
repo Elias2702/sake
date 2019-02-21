47dd6dc439695d399f32528fff9d4e297d170d7c
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2019-02-21 15:41:35 +0100

    Test envoi/réception de message client/server

--- src/client/components/hello.js
+++ src/client/components/hello.js
@@ -7,18 +7,44 @@
  */
 
 import * as React from "react";
+import io from "socket.io-client";
 
 export default class HelloWorld extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            socket: "",
+        };
+
+        this.initSocket = this.initSocket.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.disconnect = this.disconnect.bind(this);
+    }
+
+    initSocket() {
+        this.setState({
+            socket: io.connect("http://192.168.99.100:8082"),
+        });
+        this.state.socket.on("news", data => {
+            console.log(data);
+            this.state.socket.emit("my other event", {my: "data"});
+        });
+    }
+
+    sendMessage() {
+        this.state.socket.emit("test Message", {
+            message: "ceci est un message",
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>{"Hello, world!"}</h1>
+                <button onClick={this.initSocket}>{"Join"}</button>
+                <button onClick={this.sendMessage}>{"Send Message"}</button>
                 <hr />
-                <small>{"becode/bookshelf"}</small>
+                <small>{"becode/Mastermind"}</small>
             </div>
         );
     }

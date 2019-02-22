15375020e3c39c6c47082a59cfce5b396d6ca2e3
stwiertz <stephanewiertz@gmail.com>
2019-02-22 14:21:07 +0100

    ajout state messages

--- src/client/components/hello.js
+++ src/client/components/hello.js
@@ -15,6 +15,7 @@ export default class HelloWorld extends React.Component {
 
         this.state = {
             socket: "",
+            messages: "",
         };
 
         this.initSocket = this.initSocket.bind(this);
@@ -29,9 +30,16 @@ export default class HelloWorld extends React.Component {
             console.log(data);
             this.state.socket.emit("my other event", {my: "data"});
         });
+        this.state.socket.on("test Message", data => {
+            console.log(data);
+            this.setState({
+                messages: data.message,
+            });
+        });
     }
 
-    sendMessage() {
+    sendMessage(e) {
+        e.preventDefault();
         this.state.socket.emit("test Message", {
             message: "ceci est un message",
         });
@@ -44,6 +52,7 @@ export default class HelloWorld extends React.Component {
                 <button onClick={this.sendMessage}>{"Send Message"}</button>
                 <hr />
                 <small>{"becode/Mastermind"}</small>
+                <h1>{this.state.messages}</h1>
             </div>
         );
     }

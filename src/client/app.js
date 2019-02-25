c2a606852e7ca0e81603d21658904e68ea88e8b3
Elias <elias.djenidi@gmail.com>
2019-02-25 13:42:16 +0100

    starting css

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-
 import Homepage from "./components/homepage";
+import "./css/app.css";
 
 ReactDOM.render(<Homepage />, document.querySelector("#app"));

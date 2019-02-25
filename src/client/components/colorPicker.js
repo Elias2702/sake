60e991fca41c4844108f3c605e5966a0d5ddf79b
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2019-02-25 16:16:09 +0100

    list color picker

--- src/client/components/colorPicker.js
+++ src/client/components/colorPicker.js
@@ -4,9 +4,25 @@ import ColorPawn from "./ColorPawn";
 export default class ColorPicker extends React.Component {
     constructor(props) {
         super(props);
+
+        this.displayColorPawns = this.displayColorPawns.bind(this);
     }
 
+    displayColorPawns = () => {
+        let colorPawns = [];
+
+        for (let i = 0; i < this.props.availableColor; i++) {
+            colorPawns.push(<ColorPawn colorList={this.props.colorList} />);
+        }
+
+        return colorPawns;
+    };
+
     render() {
-        return <ColorPawn colorList={this.props.colorList} />;
+        return (
+            <div className="listColorPawnsContainer">
+                {this.displayColorPawns()}
+            </div>
+        );
     }
 }

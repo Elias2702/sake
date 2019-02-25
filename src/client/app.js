e22b7a5b1b0cc2f50cb30bdb8f83a0c97a92dc8e
stwiertz <stephanewiertz@gmail.com>
2019-02-25 15:28:57 +0100

    mise en place du router

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,8 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-/* import Homepage from "./components/homepage"; */
-import "./css/app.scss";
 
-/* import HelloWorld from "./components/hello"; */
-/* import Home from "./components/home"; */
+import "./scss/app.scss";
+
 import Routes from "./components/routes";
 
 ReactDOM.render(<Routes />, document.querySelector("#app"));

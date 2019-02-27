a5304d14420bd4e544960ef8c84586808e765240
kaskader <antoine.dannemark@gmail.com>
2019-02-27 11:50:58 +0100

    chat OK, methods moved to home component

--- src/client/components/chat.js
+++ src/client/components/chat.js
@@ -1,82 +1,13 @@
 import * as React from "react";
-import io from "socket.io-client";
 import MessageItem from "./messageItem";
 
-export default class HelloWorld extends React.Component {
+export default class Chat extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            socket: "",
-            playername: "",
-            message: "",
-            messages: [],
-        };
-
-        this.initSocket = this.initSocket.bind(this);
-        this.sendMessage = this.sendMessage.bind(this);
-        this.disconnect = this.disconnect.bind(this);
-        this.handleMessage = this.handleMessage.bind(this);
-        this.handlePlayerName = this.handlePlayerName.bind(this);
-    }
-
-    async initSocket(e) {
-        e.preventDefault();
-        await this.setState({
-            socket: io(),
-        });
-        this.state.socket.emit("connectionAttempt", {
-            playername: this.state.playername,
-        });
-        this.state.socket.on("connectionSuccessful", data => {
-            this.setState({
-                playernumber: data.playerNumber,
-                welcomeMessage: `Hello ${
-                    data.playerName
-                }, you can now send and recieve messages`,
-            });
-            console.log(data.welcome);
-        });
-        this.state.socket.on("Message", data => {
-            this.setState({
-                messages: data.messages,
-            });
-        });
-    }
-
-    sendMessage(e) {
-        e.preventDefault();
-        this.setState({
-            index: this.state.index + 1,
-        });
-        const newMessage = {
-            author: this.state.playername,
-            content: this.state.message,
-            id: this.state.index,
-        };
-
-        this.state.messages.push(newMessage);
-
-        this.state.socket.emit("Message", {
-            messages: this.state.messages,
-        });
-    }
-
-    disconnect() {
-        this.state.socket.emit("Disconnect", () => {
-            console.log("Disconnected");
-        });
-    }
-
-    handleMessage(event) {
-        this.setState({message: event.target.value});
-    }
-
-    handlePlayerName(event) {
-        this.setState({playername: event.target.value});
     }
 
     render() {
-        const messageItem = this.state.messages.map(message => {
+        const messageItem = this.props.messages.map(message => {
             return (
                 <MessageItem
                     key={message.id}
@@ -94,10 +25,12 @@ export default class HelloWorld extends React.Component {
                         name="message"
                         placeholder="Your message here"
                         rows="5"
-                        value={this.state.message}
-                        onChange={this.handleMessage}
+                        value={this.props.message}
+                        onChange={this.props.handleMessage}
                     />
-                    <button onClick={this.sendMessage}>{"Send Message"}</button>
+                    <button onClick={this.props.sendMessage}>
+                        {"Send Message"}
+                    </button>
                 </form>
                 <hr />
                 <div>{messageItem}</div>
--- src/client/components/home.js
+++ src/client/components/home.js
@@ -4,18 +4,41 @@ import Chat from "./chat";
 import Join from "./join";
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
         this.state = {
             socket: "",
             playername: "",
             welcomeMessage: "Please enter your name and click Join",
             message: "",
             messages: [],
-            displayChat: false,
+            isOnLine: false,
         };
 
         this.initSocket = this.initSocket.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.handleMessage = this.handleMessage.bind(this);
+    }
+
+    sendMessage(e) {
+        e.preventDefault();
+        this.setState({
+            index: this.state.index + 1,
+        });
+        const newMessage = {
+            author: this.state.playername,
+            content: this.state.message,
+            id: this.state.index,
+        };
+
+        this.state.messages.push(newMessage);
+        this.state.socket.emit("Message", {
+            messages: this.state.messages,
+        });
+    }
+
+    handleMessage(event) {
+        this.setState({message: event.target.value});
     }
 
     initSocket = async playername => {
@@ -35,7 +58,7 @@ export default class Home extends React.Component {
                     data.playerName
                 }, you have joined! You can now send and recieve messages in the chat ;)`,
             });
-            this.setState({displayChat: true});
+            this.setState({isOnLine: true});
         });
         this.state.socket.on("Message", data => {
             this.setState({
@@ -47,8 +70,16 @@ export default class Home extends React.Component {
     render() {
         let chat = "";
 
-        if (this.state.displayChat) {
-            chat = <Chat />;
+        if (this.state.isOnLine) {
+            chat = (
+                <Chat
+                    sendMessage={this.sendMessage}
+                    playerName={this.state.playername}
+                    messages={this.state.messages}
+                    message={this.state.message}
+                    handleMessage={this.handleMessage}
+                />
+            );
         }
         return (
             <div>
